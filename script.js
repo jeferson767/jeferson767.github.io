5fb029fe4806ae0768f5df2e3aacfb7baade1089
jeferson767 <jefersoncarcache0706@gmail.com>
2024-08-09 20:59:53 -0600

    tercer commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Variable para almacenar el precio inicial
+let price = 200000;
+
+// Función para decrementar el valor
+function decrementValue(element) {
+    if (element.classList.contains("disabled")) return;
+
+    price -= 10000; // Disminuye el precio en 10000 (puedes ajustar este valor)
+    updatePrice();
+
+    // Desactiva la celda de la "X" y activa la celda de la "✔"
+    element.classList.add("disabled");
+    element.nextElementSibling.classList.remove("disabled");
+}
+
+// Función para incrementar el valor
+function incrementValue(element) {
+    if (element.classList.contains("disabled")) return;
+
+    price += 10000; // Incrementa el precio en 10000 (puedes ajustar este valor)
+    updatePrice();
+
+    // Desactiva la celda de la "✔" y activa la celda de la "X"
+    element.classList.add("disabled");
+    element.previousElementSibling.classList.remove("disabled");
+}
+
+// Función para actualizar el texto en la página
+function updatePrice() {
+    document.getElementById("price").innerText = price;
+}
